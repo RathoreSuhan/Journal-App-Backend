c6bb2a91eff52ba57fb4c4839cca89290254da59
Suhan Kumar Singh <suhanrobust733@gmail.com>
2026-04-19 15:07:33 +0530

    Initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -0,0 +1,45 @@
+package net.engineeringdigest.journalApp.service;
+
+import lombok.extern.slf4j.Slf4j;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry) {
+        try{
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+        }catch(Exception e){
+            log.error("Exception ", e);
+        }
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id) {
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id) {
+        journalEntryRepository.deleteById(id);
+    }
+}
+
+
+//controller(for endpoints creation) ---> service(business logic) ---> repository(for database interaction)
